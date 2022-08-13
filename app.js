0228c10d70547fce7708b677412bfed0df813130
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-08-13 12:08:39 -0400

    finished files, working!

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-// Method override
+//Method override
 app.use(
     methodOverride(function (req, res) {
         if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
-        let method = req.body._method
-        delete req.body._method
-        return method
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
         }
     })
   )
